41dd89069d20f766eacdac08bc2e597aad4ac835
Rodrigo Rodriguez <rod19016@byui.edu>
2022-05-26 15:24:05 -0600

    addition of test cases

--- testCases.cpp
+++ testCases.cpp
@@ -18,22 +18,54 @@ void testValid(vector<User> &testCases) {
     test1.password = "ilovecats10";
     testCases.push_back(test1);
 
-    //Person1, etc.
+    // Milan
+    User test2;
+    test2.username = "Max";
+    test2.password = "A8hnu&!123";
+    testCases.push_back(test2);
+
+    // Kyle
+    User test3;
+    test3.username = "admin";
+    test3.password = "password123";
+    testCases.push_back(test3);
+
+    // Trevor
+    User test4;
+    test4.username = "john";
+    test4.password = "asdf123";
+    testCases.push_back(test4);
 }
 void testTautology(vector<User> &testCases) {
     // Rodrigo
     User test1;
     test1.username = "brayid_1978";
-    test1.password = "ilovecats10' OR 'x' = 'x";
+    test1.password = "ilovecats10' ;OR 'x' = 'x";
     testCases.push_back(test1);
 
-    //testing
-    User testing;
-    testing.username = "john_doe1";
-    testing.password = "doe12345";
-    testCases.push_back(testing);
+    //Milan
+    User test2;
+    test2.username = "Max";
+    test2.password = "x' OR 'x' = 'x";
+    testCases.push_back(test2);
 
-    //note you can also do "OR true; -- "
+    //Kyle
+    User test3;
+    test3.username = "admin";
+    test3.password = "nothing' OR '1'='1";
+    testCases.push_back(test3);
+
+    // Trevor
+    User test4;
+    test4.username = "john";
+    test4.password = "x OR 1==1"; 
+    testCases.push_back(test4);  
+
+    // Michael
+    User test5;
+    test5.username = "randomUser";
+    test5.password = "loc123 OR 'x'='1'";
+    testCases.push_back(test5);
 }
 void testUnion(vector<User> &testCases) {
     // Rodrigo
@@ -41,6 +73,30 @@ void testUnion(vector<User> &testCases) {
     test1.username = "brayid_1978";
     test1.password = "ilovecats10' UNION SELECT name FROM location; -- ";
     testCases.push_back(test1);
+
+    //Kyle
+    User test2;
+    test2.username = "admin";
+    test2.password = "nothing' UNION SELECT authenticate FROM usercredentials;";
+    testCases.push_back(test2);
+    
+    //Milan
+    User test3;
+    test3.username = "Max";
+    test3.password = "x' UNION SELECT * FROM usercreedenntials";
+    testCases.push_back(test3);
+
+    // Trevor
+    User test4;
+    test4.username = "john";
+    test4.password = "x' UNION SELECT * FROM users WHERE";
+    testCases.push_back(test4);
+    
+    // Michael
+    User test5;
+    test5.username = "randomUser";
+    test5.username = "loco123' UNION SELECT password FROM usercredentials";
+    testCases.push_back(test5);
 }
 void testAddState(vector<User> &testCases) {
     // Rodrigo
@@ -48,6 +104,30 @@ void testAddState(vector<User> &testCases) {
     test1.username = "brayid_1978";
     test1.password = "ilovecats10'; UPDATE userCredentials SET password = 'hacked' WHERE username = 'admin";
     testCases.push_back(test1);
+    
+    //Kyle
+    User test2;
+    test2.username = "admin";
+    test2.password = "nothing'; DROP TABLE usercredentials";
+    testCases.push_back(test2);
+    
+    //Milan
+    User test3;
+    test3.username = "Max";
+    test3.password = "x'; DELETE FROM usercredentials WHERE username='Admin'";
+    testCases.push_back(test3);
+
+    // Trevor
+    User test4;
+    test4.username = "john";
+    test4.password = "x'; DROP TABLES users;";
+    testCases.push_back(test4);
+    
+    //Michael 
+    User test5;
+    test5.username = "randomUser; UPDATE usercredentials SET password = loco12;";
+    test5.password = "loco123";
+    testCases.push_back(test5);
 }
 void testComment(vector<User> &testCases) {
     // Rodrigo
@@ -55,5 +135,29 @@ void testComment(vector<User> &testCases) {
     test1.username = "admin'; -- ";
     test1.password = "ilovecats10";
     testCases.push_back(test1);
+
+    //Kyle
+    User test2;
+    test2.username = "admin; --";
+    test2.password = "nothing";
+    testCases.push_back(test2);
+
+    //Milan
+    User test3;
+    test3.username = "Max'; --";
+    test3.password = "x";
+    testCases.push_back(test3);
+
+    // Trevor
+    User test4;
+    test4.username = "john; /*";
+    test4.password = "*/";
+    testCases.push_back(test4);
+
+    //Michael
+    User test5;
+    test5.username = "randomUser";
+    test5.password = ";-- loco123";
+    testCases.push_back(test5);
 }
 
